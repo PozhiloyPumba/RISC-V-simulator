c3b6ee8b18a94dc72856e7771b1237d175fdbd3a
PozhiloyPumba <ivanov.in@phystech.edu>
2023-10-11 14:52:40 +0300

    added example

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -5,10 +5,14 @@
 namespace simulator {
 int Main(int argc, const char **argv)
 {
+    if (argc != 2)
+        return -1;
+
     mem::VirtualMem *vmem = mem::VirtualMem::CreateVirtualMem();
-    uintptr_t entry_point = vmem->LoadElfFile("ahahahahhahahha.out");
+    uintptr_t entry_point = vmem->LoadElfFile(argv[1]);
     core::Hart hart(vmem, entry_point);
     hart.RunImpl();
+    mem::VirtualMem::Destroy(vmem);
     return 0;
 }
 }  // namespace simulator
